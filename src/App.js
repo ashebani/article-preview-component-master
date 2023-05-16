887a92be1def39686d59d833b159d20779cac097
ashebani <azizoo2001.as@gmail.com>
2023-05-16 18:59:48 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import avatar from "./images/avatar-michelle.jpg";
+import share_icon from "./images/icon-share.svg";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="image_container"></div>
+
+      <div className="container">
+        <div className="text_container">
+          <h4 className="title">
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
+          </h4>
+          <p className="info">
+            Ever been in a room and felt like something was missing? Perhaps it
+            felt slightly bare and uninviting. I’ve got some simple tips to help
+            you make any room feel complete.
+          </p>
+        </div>
+
+        <div className="social_share">
+          <div className="social">
+            <div className="social_info">
+              <img src={avatar} alt="" className="avatar" />
+              <p>
+                <span>Michelle Appleton</span>28 Jun 2020
+              </p>
+            </div>
+            <img
+              src={share_icon}
+              onClick={() => setOpen(!open)}
+              alt="Share"
+              className="share_icon"
+            />
+          </div>
+
+          <div className={`share ${open ? "" : "hidden"}`}>
+            <div className="social_info">
+              <p>Share</p>
+              <img
+                srcSet={require("./images/icon-facebook.svg").default}
+                alt="facebook"
+              />
+              <img
+                srcSet={require("./images/icon-twitter.svg").default}
+                alt="twitter"
+              />
+              <img
+                srcSet={require("./images/icon-pinterest.svg").default}
+                alt="pinterest"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
